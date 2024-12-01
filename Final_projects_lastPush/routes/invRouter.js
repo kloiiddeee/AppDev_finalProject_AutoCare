0259eb77e5129dcb7eea3bad26d0591fca21327c
kloiiddeee.com <161782890+kloiiddeee@users.noreply.github.com>
2024-12-01 21:33:31 +0800

    Update invRouter.js

--- Final_projects_lastPush/routes/invRouter.js
+++ Final_projects_lastPush/routes/invRouter.js
@@ -2,19 +2,15 @@ const express = require('express');
 const router = express.Router();
 const inventoryController = require('../controllers/invController');
 
-// Route to add new inventory item
+
 router.post('/addInventory', inventoryController.addInventory);
 
-// Route to get all inventory items
 router.get('/get-all-inventory', inventoryController.getInventory);
 
-// Route to get a single inventory item by ID
-router.get('/updateInventory/:id', inventoryController.getInventoryItem);
+router.get('/updateInv/:id', inventoryController.getInventoryItem);
 
-// Route to update an inventory item
-router.post('/updateInventory/:id', inventoryController.updateInventory);
+router.post('/updateInv/:id', inventoryController.updateInventory);
 
-// Route to delete an inventory item
-router.get('/deleteInventory/:id', inventoryController.deleteInventory);
+router.get('/deleteInv/:id', inventoryController.deleteInventory);
 
 module.exports = router;
